601b6a8b8054e31595313b0e946bfbd2e5a02b94
Eric <emjyoon@gmail.com>
2022-08-30 20:06:02 -0700

    Address floating point error; add tests (secret text on hub)

--- src/ATM.java
+++ src/ATM.java
@@ -16,6 +16,17 @@ public class ATM {
 		System.out.println(atm.checkBalance(123) == 7);
 		System.out.println(!atm.withdrawMoney(123, 8));
 		
+		// Ensure openAccount only works once existing account is closed
+		atm.openAccount(123, 99999);
+		System.out.println(atm.checkBalance(123) != 99999);
+		atm.closeAccount(123); // fails because balance is not zero
+		atm.openAccount(123, 99999);
+		System.out.println(atm.checkBalance(123) != 99999);
+		atm.withdrawMoney(123, atm.checkBalance(123));
+		atm.closeAccount(123);
+		atm.openAccount(123, 99999);
+		System.out.println(atm.checkBalance(123) == 99999);
+		
 	}
 	public ATM() {
 		accounts = new HashMap<Integer, Double>();
@@ -34,7 +45,7 @@ public class ATM {
 	}
 	public double checkBalance(int accountNum) {
 		if(!accounts.containsKey(accountNum)) return 0;
-		return accounts.get(accountNum);
+		return (double)Math.round(accounts.get(accountNum) * 100) / 100; // Round to fix floating point error
 	}
 	public boolean depositMoney(int accountNum, double amount) {
 		if(amount < 0) return false;
